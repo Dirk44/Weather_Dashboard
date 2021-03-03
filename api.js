40fb2a4c8c90c359473084854312555998574aa9
Derrick deVilliers <derrickd44@gmail.com>
2021-03-03 11:51:05 -0500

    Reworking app. have current weather working, Working on forecast.

--- api.js
+++ api.js
@@ -1,28 +1,28 @@
 
-const key = "f33cb88be12584833951fc21673d4cde";
+// const key = "f33cb88be12584833951fc21673d4cde";
 
 
-const requestedCity = async (city) => {
-    const callUrl = "http://api.openweathermap.org/data/2.5/weather";
-    const query1 = `?q=${city}&units=imperial&appid=${key}`;
+// const requestedCity = async (city) => {
+//     const callUrl = "http://api.openweathermap.org/data/2.5/weather";
+//     const query1 = `?q=${city}&units=imperial&appid=${key}`;
     
-    const response = await fetch(callUrl + query1);
+//     const response = await fetch(callUrl + query1);
 
-    const data = await response.json();
-    return data;
+//     const data = await response.json();
+//     return data;
     
 
-}
+// }
 
-const updateForecast = async (city) => {
-    const callUrl2 = "http://api.openweathermap.org/data/2.5/forecast";
-    const query2 = `?q=${city}&units=imperial&appid=${key}`;
+// const updateForecast = async (city) => {
+//     const callUrl2 = "http://api.openweathermap.org/data/2.5/forecast";
+//     const query2 = `?q=${city}&units=imperial&appid=${key}`;
 
-    const responseFore = await fetch(callUrl2 + query2);
+//     const responseFore = await fetch(callUrl2 + query2);
 
-    const fore = await responseFore.json();
-    return fore;
+//     const fore = await responseFore.json();
+//     return fore;
 
-}
+// }
 
 // const forecastQuery = "http://api.openweathermap.org/data/2.5/forecast" + `?q=${city}&units=imperial&appid=${key}`;
--- index.js
+++ index.js
@@ -1,117 +1,162 @@
 
 
 
-var searchVal = document.getElementById("search-value");
-var searchBtn = document.getElementById("search-button");
-var current = document.getElementById("today");
-var cityName = document.getElementById("city");
-var currentDate = document.getElementById("date");
-var currentTemp = document.getElementById("temp");
-var currentHumidity = document.getElementById("humidity");
-var currentWind = document.getElementById("wind");
-var searchedCityList = document.getElementById("searched-cities");
-var tempMin = document.getElementById("temp-min");
-var tempMax = document.getElementById("temp-max");
-var imgIcon = document.getElementById("icon");
+const searchBtn = document.getElementById("search-button");
+const current = document.getElementById("today");
+const cityName = document.getElementById("city");
+const date = document.getElementById("date");
+const currentTemp = document.getElementById("temp");
+const currentHumidity = document.getElementById("humidity");
+const currentWind = document.getElementById("wind");
+const tempMin = document.getElementById("temp-min");
+const tempMax = document.getElementById("temp-max");
+const imgIcon = document.getElementById("icon");
+const history = document.getElementById("history");
 
-
-var searchedCities = JSON.parse(localStorage.getItem("savedContent")) || [];
+const searchedCities = JSON.parse(localStorage.getItem("savedContent")) || [];
 
 var currentDate = new Date().toLocaleDateString("en-US");
 
+const key = "f33cb88be12584833951fc21673d4cde";
+const callUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+const callUrl2 = "http://api.openweathermap.org/data/2.5/forecast?q=";
 
-// $(searchBtn).on("click", (event) => {
-//     event.preventDefault();
-//     const searchedCity = searchVal.value.trim();
-//     console.log(searchedCity);
+// function load() {
 
-//     $(searchVal).val("");
-
-//     requestedCity(searchedCity)
-//         .then((data) => {
-//             updateWeather(data);
-//             console.log(data);
-//         })
-
-//         .catch((err) => {
-//             console.log(err);
-//         });
-        
-//         $(searchBtn).on("click", (event) => {
-//             event.preventDefault();
-//             const searchedCity = searchVal.value.trim();
-//             console.log(searchedCity);
-        
-//             $(searchVal).val("");
-        
-//         updateForecast(searchedCity)
-//         .then((fore) => {
-//             appendForecast(fore);
-//             console.log(fore);
-//         })  
-  
-//     .catch((err) => {
-//         console.log(err);
-//     });
-
-// })
-// })
+// }
 
 
+$(document).ready(function () {
 
-updateWeather = (city) => {
-    cityName.textContent = city.name + " (" + currentDate + ")";
-    const iconName = city.weather[0].icon;
-    const appIcon = `<img src="http://openweathermap.org/img/wn/${iconName}@2x.png"id="icons">`;
-    currentTemp.textContent = ("Temperature:  " + city.main.temp + ("º F"));
-    currentHumidity.textContent = ("Humidity:  " + city.main.humidity + ("%"));
-    currentWind.textContent = ("Wind Speed:  " + city.wind.speed + (" mph"));
-    tempMin.textContent = ("Low:  " + city.main.temp_min + ("º F"));
-    tempMax.textContent = ("High:  " + city.main.temp_max + ("º F"));
-    $(cityName).append(appIcon);
+    
+    $(searchBtn).on("click", function (event) {
+        const searchVal = document.getElementById("search-value");
+        event.preventDefault();
+        const searchedCity = $(searchVal).val();
+        console.log(searchedCity);
 
-    console.log(iconName);
+        $(searchVal).val("");
 
-}
+
+        getWeather(searchedCity);
+    });
+       
 
 
 
 
-function updatedForecast(searchedCity) {
+        function getWeather(searchVal) {
+            $.ajax({
+                type: "GET",
+                url: callUrl + searchVal + `&units=imperial&appid=${key}`,
+                dataType: "json"
+            }).then(function (city) {
+                cityName.textContent = city.name + "(" + currentDate + ")";
+                
+                const iconName = city.weather[0].icon;
+                const appIcon = `<img src="http://openweathermap.org/img/wn/${iconName}@2x.png"id="icons">`;
+                
+                currentTemp.textContent = ("Temperature:  " + city.main.temp + ("º F"));
+                currentHumidity.textContent = ("Humidity:  " + city.main.humidity + ("%"));
+                currentWind.textContent = ("Wind Speed:  " + city.wind.speed + (" mph"));
+                tempMin.textContent = ("Low:  " + city.main.temp_min + ("º F"));
+                tempMax.textContent = ("High:  " + city.main.temp_max + ("º F"));
+                $(cityName).append(appIcon);
 
-    $.ajax({
-        type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=f33cb88be12584833951fc21673d4cde",
-        dataType: "json"
-    }).then(function(data) {
+                console.log(city);
+                updatedForecast(searchVal);
 
-                //loop over foreacsted array
+            })
+        }
+
+        // requestedCity(searchedCity)
+        //     .then((data) => {
+        //         updateWeather(data);
+        //         console.log(data);
+        //     })
+
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+        // })
+
+        //         $(searchBtn).on("click", (event) => {
+        //             event.preventDefault();
+        //             const searchedCity = searchVal.value.trim();
+        //             console.log(searchedCity);
+
+        //             $(searchVal).val("");
+
+        //         updateForecast(searchedCity)
+        //         .then((fore) => {
+        //             appendForecast(fore);
+        //             console.log(fore);
+        //         })  
+
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+
+        // })
+        // })
+
+
+
+        // updateWeather = (city) => {
+        //     cityName.textContent = city.name + " (" + currentDate + ")";
+        //     const iconName = city.weather[0].icon;
+        //     const appIcon = `<img src="http://openweathermap.org/img/wn/${iconName}@2x.png"id="icons">`;
+        //     currentTemp.textContent = ("Temperature:  " + city.main.temp + ("º F"));
+        //     currentHumidity.textContent = ("Humidity:  " + city.main.humidity + ("%"));
+        //     currentWind.textContent = ("Wind Speed:  " + city.wind.speed + (" mph"));
+        //     tempMin.textContent = ("Low:  " + city.main.temp_min + ("º F"));
+        //     tempMax.textContent = ("High:  " + city.main.temp_max + ("º F"));
+        //     $(cityName).append(appIcon);
+
+        //     console.log(iconName);
+
+        // }
+
+
+
+
+        function updatedForecast(searchVal) {
+
+            $.ajax({
+                type: "GET",
+                url: callUrl2 + searchVal + `&units=imperial&appid=${key}`,
+                dataType: "json",
+                success: function (data) {
+
+                //loop over forecasted array
                 for (var i = 0; i < data.list.length; i++) {
-                            console.log("After array");
+                    console.log(data);
                     if (data.list[i].dt_text.indexOf("12:00:00") !== -1) {
 
                         //creating html elements
                         const col = $("div>").addClass("col-md-2");
                         const card = $("<div>").addClass("card bg-primary text-white");
                         const cardBody = $("<div>").addClass("card-body p-2");
-                        const city = $("<div>").addClass("card-title").text(currentDate(data.list[i].dt_text).toLocaleDateString());
+                        const city = $("<div>").addClass("card-title").text(data.list[i].dt_text).toLocaleDateString();
                         const weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
                         const temp = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + "º F");
                         const humidity = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + ("%"));
 
                         //add to page
                         col.append(card.append(cardBody.append(city, weatherIcon, temp, humidity)));
-                        $(".row").append(col);
+                        $("#forecast .row").append(col);
+
+                        
                     }
+                    
                 }
-                console.log(searchVal);
-            });
+                
+            }
+        });
         }
-        
-updatedForecast();
 
-
-
+    
+})
 
 
 
